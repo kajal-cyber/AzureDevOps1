42018d9892f2b2156d68b2b683656dbd0612c6dc
kajal-cyber <119293712+kajal-cyber@users.noreply.github.com>
2023-03-09 15:18:14 +0530

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello from GG again--hihihiihhiiiiii again!!!!!";
+        return "Hello from GG again--hi again!!!!!";
     }
 
     public static void main(String[] args) {
